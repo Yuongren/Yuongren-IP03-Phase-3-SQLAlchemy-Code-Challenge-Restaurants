e8b9c602d2438a341cd54e2576f7536b06244474
gitYoungren <youngrengitonga@gmail.com>
2024-01-03 06:50:23 +0300

    done

---
+++ lib/models.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+sys.path.append(os.getcwd)
+
+from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer, ForeignKey)
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///db/concerts.db', echo=True)
+
+
+class Band(Base):
+    __tablename__ = 'bands'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    hometown = Column(String())
+
+
+    def __repr__(self):
+        return f'Band: {self.name}'
+
+
+class Venue(Base):
+    __tablename__ = 'venues'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String())
+    city = Column(String())
+
+    
+    def __repr__(self):
+        return f'Venue: {self.title}'
